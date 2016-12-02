47d576acb0e1661f93f12582b77f9fa2129c59b9
Thilo Will <thilow@microsoft.com>
2016-12-02 16:41:13 +0100

    added test_random_sample_without_replacement

--- bindings/python/cntk/ops/tests/random_sample_test.py
+++ bindings/python/cntk/ops/tests/random_sample_test.py
@@ -52,18 +52,18 @@ def test_random_sample_inclusion_frequency(weights, num_samples, allow_duplicate
 
 # BUGBUG add test for random_sample(...) too.
 
-RANDOM_SAMPLE_TEST_CASES = [
+RANDOM_SAMPLE_TEST_CASES_WITH_REPLACEMENT = [
     ([1., 3., 5., 1.],  1000, 0.03, False),
     ([1., -1.],  100, 0.0, True),
 ]
-@pytest.mark.parametrize("weights, num_samples,  tolerance, raises_exception", RANDOM_SAMPLE_TEST_CASES)
-def test_random_sample_with_replacment(weights, num_samples,  tolerance, raises_exception, device_id, precision):
+@pytest.mark.parametrize("weights, num_samples,  tolerance, raises_exception", RANDOM_SAMPLE_TEST_CASES_WITH_REPLACEMENT)
+def test_random_sample_with_replacement(weights, num_samples,  tolerance, raises_exception, device_id, precision):
 
     weights = AA(weights);
     expected_relative_frequency = weights / np.sum(weights)
     num_calls = 10;
     identity = np.identity(weights.size)
-    allow_duplicates = True
+    allow_duplicates = True # sample with replacement
 
     if raises_exception:
         with pytest.raises(ValueError):
@@ -78,4 +78,30 @@ def test_random_sample_with_replacment(weights, num_samples,  tolerance, raises_
         observed_relative_frequency = observed_frequency / (num_calls * num_samples)
         assert np.allclose(observed_relative_frequency, expected_relative_frequency, atol=tolerance)
 
+        
+
+RANDOM_SAMPLE_TEST_CASES_WITHOUT_REPLACEMENT = [
+    ([1., 3, 50., 1., 0.], 4, (0.25, 0.25, 0.25, 0.25, 0), 0.03, False),
+    ([1., -1.],  1, None,   0.0, True),
+]
+@pytest.mark.parametrize("weights, num_samples, expected_relative_frequency, tolerance, raises_exception", RANDOM_SAMPLE_TEST_CASES_WITHOUT_REPLACEMENT)
+def test_random_sample_without_replacement(weights, num_samples, expected_relative_frequency, tolerance, raises_exception, device_id, precision):
+
+    weights = AA(weights);
+    num_calls = 1;
+    identity = np.identity(weights.size)
+    allow_duplicates = False # sample without replacement
+
+    if raises_exception:
+        with pytest.raises(ValueError):
+            result = random_sample(weights, num_samples, allow_duplicates)
+            result.eval()
+    else:
+        observed_frequency = np.zeros_like(weights)
+        for i in range(0, num_calls):
+            result = random_sample(weights, num_samples, allow_duplicates)
+            denseResult = times(result, identity)
+            observed_frequency += np.sum(denseResult.eval(), 0)
+        observed_frequency /= (num_calls * num_samples)
+        assert np.allclose(observed_frequency, expected_relative_frequency, atol=tolerance)
 
