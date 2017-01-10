e6c4483a88693472d856dfc1899e838f69689ec2
Thilo Will <thilow@microsoft.com>
2017-01-10 16:31:43 +0100

    Cleanup in word-rnn2.py

--- Examples/Text/WordBasedLM/word-rnn2.py
+++ Examples/Text/WordBasedLM/word-rnn2.py
@@ -23,41 +23,56 @@ hidden_dim = 256
 num_layers = 1
 minibatch_size = 257 # also how much time we unroll the RNN for
 
-def cross_entropy_with_sampled_softmax(output_vector, target_vector, num_samples, sampling_weights, vocab_dim, hidden_dim, allow_duplicates=False, name=''):
+# model subgraph computing cross-entropy with sampled softmax
+def cross_entropy_with_sampled_softmax(
+    hidden_vector,           # Node providing the output of the recurrent layers
+    target_vector,           # Node providing the expected labels (as sparse vectors)
+    num_samples,             # Number of samples to use for sampled softmax
+    sampling_weights,        # Node providing weights to be used for the weighted sampling
+    vocab_dim,               # Vocabulary size
+    hidden_dim,              # Row-dimension of the hidden vector
+    allow_duplicates = False # Boolean flag to control wheather to use sampling with replacemement (allow_duplicates == True) or without replacement.
+    ):
     bias = C.Parameter(shape = (vocab_dim, 1), init = C.init_bias_default_or_0, name='B')
     weights = C.Parameter(shape = (vocab_dim, hidden_dim), init = C.init_default_or_glorot_uniform, name='E')
     sample_selector = C.random_sample(sampling_weights, num_samples, allow_duplicates) # sparse matrix [num_samples * vocab_size]
-    inclusion_probs = C.random_sample_inclusion_frequency(sampling_weights, num_samples, allow_duplicates) # dense row [1 * vocal_size]
-    log_prior = C.log(inclusion_probs) # dense row [1 * vocal_size]
+    inclusion_probs = C.random_sample_inclusion_frequency(sampling_weights, num_samples, allow_duplicates) # dense row [1 * vocab_size]
+    log_prior = C.log(inclusion_probs) # dense row [1 * vocab_dim]
 
-    wS = C.times(sample_selector, weights, name='wS') # [num_samples * hidden_dim] 
-    zS = C.times_transpose(wS, output_vector, name='zS1') + C.times(sample_selector, bias, name='zS2') - C.times_transpose (sample_selector, log_prior, name='zS3')# [numSamples]
+    wS = C.times(sample_selector, weights, name='wS') # [num_samples * hidden_dim]
+    zS = C.times_transpose(wS, hidden_vector, name='zS1') + C.times(sample_selector, bias, name='zS2') - C.times_transpose (sample_selector, log_prior, name='zS3')# [num_samples]
 
-    # Getting the weight vector for the true label. Dimension numHidden
-    wT = C.times(target_vector, weights, name='wT') # [1 * numHidden]
-    zT = C.times_transpose(wT, output_vector, name='zT1') + C.times(target_vector, bias, name='zT2') - C.times_transpose(target_vector, log_prior, name='zT3') # [1]
+    # Getting the weight vector for the true label. Dimension hidden_dim
+    wT = C.times(target_vector, weights, name='wT') # [1 * hidden_dim]
+    zT = C.times_transpose(wT, hidden_vector, name='zT1') + C.times(target_vector, bias, name='zT2') - C.times_transpose(target_vector, log_prior, name='zT3') # [1]
 
     zSReduced = C.reduce_log_sum(zS)
 
+    # Compute the cross entropy that is used for training.
     cross_entropy_on_samples = zSReduced - zT
 
-    # for testing purposes setup cross entropy with full softmax
-    z = C.times_transpose(weights, output_vector) + bias
+    # For applying the model we also output a node providing the input for the full softmax
+    z = C.times_transpose(weights, hidden_vector) + bias
     z = C.reshape(z, shape = (vocab_dim))
     zReduced = C.reduce_log_sum(zS)
-    cross_entropy_with_full_softmax = zReduced - output_vector
+    cross_entropy_with_full_softmax = zReduced - hidden_vector
 
     zSMax = C.reduce_max(zS)
     error_on_samples = C.less(zT, zSMax)
     return (z, cross_entropy_on_samples, error_on_samples, cross_entropy_with_full_softmax)
 
+# Computes exp(z[index])/np.sum(exp[z]) for a one-dimensional numpy array in an numerically stable way.
 def softmax(z, index):
     max_z = np.max(z)
     return np.exp(z[index] - max_z)/np.sum(np.exp(z - max_z))
 
-# computes the average cross entropy (in nats) for the specified text
-def compute_average_cross_entropy(model_node, word_to_ix, word_ids, index_of_prime_word, vocab_dim):
-    priming_input = C.one_hot([[int(index_of_prime_word)]], vocab_dim)
+# Computes the average cross entropy (in nats) for the specified text
+def compute_average_cross_entropy(
+    model_node,          # node computing the inputs to softmax
+    word_ids,            # Sequence for which to compute the cross-entropy. Sequence is specified as a list of (word) ids.
+    index_of_prime_word, # Index (of word) to prime the model
+    vocab_dim):
+    priming_input = C.one_hot([[index_of_prime_word]], vocab_dim)
     arguments = (priming_input, [True])
 
     total_cross_entropy = 0.0
@@ -71,8 +86,17 @@ def compute_average_cross_entropy(model_node, word_to_ix, word_ids, index_of_pri
     return total_cross_entropy / len(word_ids)
 
 
-# Generate sequences from the model
-def sample(root, ix_to_word, vocab_dim, word_to_ix, prime_text='', use_hardmax=True, length=100, alpha=1.0):
+# Generate sequences from the model.
+def sample(
+    model_node,       # node computing the inputs to softmax
+    ix_to_word,       # Dictionary mapping indices to tokens
+    word_to_ix,       # Dictionary mapping tokens to indices
+    vocab_dim,        # size of vocabulary
+    prime_text='',    # text for priming the model
+    use_hardmax=True, # Tells wehter hardmax should be used
+    length=100,       # Length of sequence to generate
+    alpha=1.0         # scaling exponent used for tweaking the probablities coming from the model. Used to control diversity of generated sequences.
+    ):
 
     def sample_word(p):
         if use_hardmax:
@@ -104,7 +128,7 @@ def sample(root, ix_to_word, vocab_dim, word_to_ix, prime_text='', use_hardmax=T
     
     # loop through prime text
     for i in range(plen):
-        p = root.eval(arguments)
+        p = model_node.eval(arguments)
         # reset
         if i < plen-1:
             idx = word_to_ix[words[i+1]]
@@ -118,7 +142,7 @@ def sample(root, ix_to_word, vocab_dim, word_to_ix, prime_text='', use_hardmax=T
     
     # loop through length of generated text, sampling along the way
     for i in range(length-plen):
-        p = root.eval(arguments)
+        p = model_node.eval(arguments)
         idx = sample_word(p)
         output.append(idx)
         x = C.one_hot([[int(idx)]], vocab_dim)
@@ -153,7 +177,7 @@ def create_inputs(vocab_dim):
 def train_lm(training_file, word_to_ix_file_path, sampling_weights_file_path, total_num_epochs, softmax_sample_size, alpha, input_test_text_file):
 
     # load the data and vocab
-    data, word_to_ix, ix_to_char, data_size, vocab_dim = load_data_and_vocab(training_file, word_to_ix_file_path)
+    data, word_to_ix, ix_to_word, data_size, vocab_dim = load_data_and_vocab(training_file, word_to_ix_file_path)
     
     # load the test data
     word_ids_test = text_file_to_word_ids(input_test_text_file, word_to_ix)
@@ -171,10 +195,10 @@ def train_lm(training_file, word_to_ix_file_path, sampling_weights_file_path, to
     weights = load_sampling_weights(sampling_weights_file_path)
     smoothed_weights = np.float32( np.power(weights, alpha))
     sampling_weights = C.reshape(C.Constant(smoothed_weights), shape = (1,vocab_dim))
-    model, ce, error_on_samples, cross_entropy_with_full_softmax = cross_entropy_with_sampled_softmax(z, label_sequence, softmax_sample_size, sampling_weights, vocab_dim, hidden_dim, name = 'sampled_softmax')
+    model, ce, error_on_samples, cross_entropy_with_full_softmax = cross_entropy_with_sampled_softmax(z, label_sequence, softmax_sample_size, sampling_weights, vocab_dim, hidden_dim)
 
     # Instantiate the trainer object to drive the model training
-    lr_per_sample = learning_rate_schedule(0.001, UnitType.sample)
+    lr_per_sample = learning_rate_schedule(0.01, UnitType.sample)
     momentum_time_constant = momentum_as_time_constant_schedule(1100)
     clipping_threshold_per_sample = 5.0
     gradient_clipping_with_truncation = True
@@ -219,9 +243,9 @@ def train_lm(training_file, word_to_ix_file_path, sampling_weights_file_path, to
         num_samples_between_progress_report = 1000
         if num_trained_samples_since_last_report >= num_samples_between_progress_report:
             progress_printer.update_with_trainer(trainer, with_metric=True) # log progress
-            print(sample(model, ix_to_char, vocab_dim, word_to_ix))
+            print(sample(model, ix_to_word, word_to_ix, vocab_dim))
             print("start....")
-            average_cross_entropy = compute_average_cross_entropy(model, word_to_ix, word_ids_test, 1, vocab_dim)
+            average_cross_entropy = compute_average_cross_entropy(model, word_ids_test, 1, vocab_dim)
             print("average cross entropy:" + str(average_cross_entropy))
             num_samples_between_progress_report = 0
 
@@ -234,9 +258,9 @@ def load_and_sample(model_filename, word_to_ix_file_path, prime_text='', use_har
     model = load_model(model_filename)
     
     # load the vocab
-    word_to_ix, ix_to_char = load_word_to_ix(word_to_ix_file_path)
+    word_to_ix, ix_to_word = load_word_to_ix(word_to_ix_file_path)
     
-    output = sample(model, ix_to_char, len(word_to_ix), word_to_ix, prime_text=prime_text, use_hardmax=use_hardmax, length=length, alpha=alpha)
+    output = sample(model, ix_to_word, word_to_ix, len(word_to_ix), prime_text=prime_text, use_hardmax=use_hardmax, length=length, alpha=alpha)
     
     ff = open('output.txt', 'w', encoding='utf-8')
     ff.write(output)
@@ -249,7 +273,7 @@ if __name__=='__main__':
 
     num_epochs = 32
     input_training_text_file = "ptbData/ptb.train.txt"
-    input_test_text_file = "ptbData/ptb.test.txt"
+    input_test_text_file = "ptbData/ptb.test.subset.txt"
     input_word2index_file = "ptbData/ptb.word2id.txt"
     input_sampling_weights = "ptbData/ptb.freq.txt"
     
